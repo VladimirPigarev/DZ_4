a6bfcad1d332757301f5fce043673daac1a19dc8
VladimirPigarev <109297010+VladimirPigarev@users.noreply.github.com>
2022-08-17 21:16:10 +0300

    Написал код для задачи 29

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+using static System.Console;
+Clear();
+
+int[] array = new int[8];
+fillArray(array);
+printArray(array);
+
+
+
+
+void fillArray(int[] arrnew)
+{
+    for(int i = 0; i < arrnew.Length; i++)
+    {
+        arrnew[i] = new Random().Next(0, 8);
+    }
+}
+
+void printArray(int[] arrnew)
+{
+    Write("[");
+    for(int i = 0; i < arrnew.Length; i++)
+    {
+        Write($"{arrnew[i]}");
+        if(i < arrnew.Length-1) Write(", ");
+    }
+    WriteLine("]");
+}
