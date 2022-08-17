6ac8ed705d0462c3d79c91e572120baa46853bf7
VladimirPigarev <109297010+VladimirPigarev@users.noreply.github.com>
2022-08-17 20:35:13 +0300

    Написал код для решения задачи № 27

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,22 @@
+﻿// See Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+using static System.Console;
+Clear();
+WriteLine("Введите число");
+int number = Convert.ToInt32(ReadLine());
+int result = SumOfTheDigitNumber(number);
+WriteLine(result);
+
+
+
+
+int SumOfTheDigitNumber(int number1)
+{
+    int result1 = 0;
+    while(number1 > 0)
+    {
+        result1 = result1 + number1 % 10;
+        number1 = number1 / 10;
+    }
+    return result1;
+}
